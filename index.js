79728c76dc7e8bc13032aa649ad412150e644fd9
Monika <monika_streljajeva@hotmail.com>
2020-04-06 15:55:41 +0200

    display tweets in html

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const T = new Twit({
     consumer_secret: process.env.consumer_secret,
     access_token: process.env.access_token,
     access_token_secret: process.env.access_token_secret,
-    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
     strictSSL: true, // optional - requires SSL certificates to be valid.
 })
 
--- public/main.js
+++ public/main.js
@@ -1,7 +1,11 @@
 const socket = io();
-
 const tweets = document.getElementById('tweets')
 
 socket.on('tweet', function (data) {
-    console.log(data)
+    console.log(data.text)
+
+    const newTweet = document.createElement("li");
+    newTweet.textContent = data.text;
+
+    tweets.appendChild(newTweet);
 })
