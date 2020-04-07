f4de0130ed8c48a8bd83e74880547bda5e52d869
Monika <monika_streljajeva@hotmail.com>
2020-04-07 16:45:10 +0200

    add trump command to show a random tweet of him

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+const Twit = require("twit");
 const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
 require("dotenv").config();
+
+const T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+  strictSSL: true, // optional - requires SSL certificates to be valid.
+});
 
 app.use(
   bodyParser.urlencoded({
@@ -18,7 +27,38 @@ app.get("/", function (req, res) {
 });
 
 io.sockets.on("connection", function (socket) {
-  console.log("connected");
+  console.log("A user connected");
+
+  //add username
+  socket.on("newuser", function (username) {
+    socket.username = username;
+    io.sockets.emit("newName", socket.id, socket.username);
+  });
+
+  //send message
+  socket.on("chat message", function (msg) {
+    let message = msg.split(" ");
+
+    if (message.includes("!trump")) {
+      T.get(
+        "statuses/user_timeline",
+        { screen_name: "potus", count: 200 },
+        function (err, data, response) {
+          console.log(data.length);
+          io.emit("chat message", msg);
+
+          const randomTweet = data[Math.floor(Math.random() * data.length)];
+          io.emit("chat message", randomTweet.text);
+        }
+      );
+    } else {
+      io.emit("chat message", socket.username + ": " + msg);
+    }
+  });
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
 });
 
 http.listen(process.env.PORT || 3000);
--- public/main.js
+++ public/main.js
@@ -1,12 +1,32 @@
 const socket = io();
-const tweets = document.getElementById('tweets')
+var form = document.querySelector("#chat");
+const addUsername = document.querySelector("#addUsername");
+const message = document.querySelector(".messages");
 
-socket.on('chat', function (data) {
-    console.log(data.text)
-    console.log(data.lang)
+//add username
+addUsername.addEventListener("submit", (e) => {
+  const username = document.querySelector("#username");
+  e.preventDefault();
+  socket.emit("newuser", username.value);
+  console.log(username.value);
+  addUsername.parentNode.removeChild(addUsername);
+});
 
-    const newMessage = document.createElement("li");
-    newMessage.textContent = data;
+// get message value
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  socket.emit("chat message", m.value);
+  m.value = "";
+  return false;
+});
 
-    tweets.appendChild(newTweet);
-})
+//show message
+socket.on("chat message", function (msg) {
+  const newMessage = document.createElement("li");
+  newMessage.textContent = msg;
+  message.appendChild(newMessage);
+});
+
+// socket.on("newName", function (id, name) {
+//   console.log(id);
+// });
