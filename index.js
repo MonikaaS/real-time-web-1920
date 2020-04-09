f32d52ec40a53919f7514b8c5e0a9aeb191bee6e
Monika <monika_streljajeva@hotmail.com>
2020-04-09 17:21:28 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const Twit = require("twit");
 const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
 require("dotenv").config();
-
-const T = new Twit({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret,
-  strictSSL: true, // optional - requires SSL certificates to be valid.
-});
 
 app.use(
   bodyParser.urlencoded({
@@ -32,28 +23,26 @@ io.sockets.on("connection", function (socket) {
   //add username
   socket.on("newuser", function (username) {
     socket.username = username;
-    io.sockets.emit("newName", socket.id, socket.username);
+    io.sockets.emit("newName", username);
+
+    //send welcome message
+    socket.emit(
+      "server message",
+      `SERVER: Welcome ${username} !!!`
+    );
+
+    // Show everyone else who has joined
+    socket.broadcast.emit('server message', `SERVER: User ${username} connected.`);
   });
 
   //send message
   socket.on("chat message", function (msg) {
-    let message = msg.split(" ");
+    io.emit("chat message", socket.username + ": " + msg);
+  });
 
-    if (message.includes("!trump")) {
-      T.get(
-        "statuses/user_timeline",
-        { screen_name: "potus", count: 200 },
-        function (err, data, response) {
-          console.log(data.length);
-          io.emit("chat message", msg);
-
-          const randomTweet = data[Math.floor(Math.random() * data.length)];
-          io.emit("chat message", randomTweet.text);
-        }
-      );
-    } else {
-      io.emit("chat message", socket.username + ": " + msg);
-    }
+  //fetch the data when a user uses a command
+  socket.on("fetch villager", function () {
+    io.emit("command message", `er is een command gebruikt`);
   });
 
   socket.on("disconnect", function () {
--- public/main.js
+++ public/main.js
@@ -1,21 +1,30 @@
 const socket = io();
-var form = document.querySelector("#chat");
+const form = document.querySelector("#chat");
 const addUsername = document.querySelector("#addUsername");
+const username = document.querySelector("#username");
 const message = document.querySelector(".messages");
 
 //add username
-addUsername.addEventListener("submit", (e) => {
-  const username = document.querySelector("#username");
+addUsername.addEventListener("submit", function (e) {
   e.preventDefault();
   socket.emit("newuser", username.value);
-  console.log(username.value);
   addUsername.parentNode.removeChild(addUsername);
 });
 
 // get message value
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  socket.emit("chat message", m.value);
+
+  console.log(m.value)
+
+  if (m.value.includes("!villager")) {
+    let villager = m.value.replace(/!villager /g, '');
+
+    console.log(villager)
+    socket.emit("fetch villager", message);
+  } else {
+    socket.emit("chat message", m.value);
+  }
   m.value = "";
   return false;
 });
@@ -23,10 +32,27 @@ form.addEventListener("submit", (e) => {
 //show message
 socket.on("chat message", function (msg) {
   const newMessage = document.createElement("li");
+
   newMessage.textContent = msg;
   message.appendChild(newMessage);
 });
 
-// socket.on("newName", function (id, name) {
-//   console.log(id);
-// });
+//show server message
+socket.on("server message", function (serverMsg) {
+  const newServerMessage = document.createElement("li");
+  newServerMessage.textContent = serverMsg;
+  message.appendChild(newServerMessage);
+});
+
+//show command message
+socket.on("command message", function (command) {
+  const commandMessage = document.createElement("li");
+  commandMessage.textContent = command;
+  message.appendChild(commandMessage);
+});
+
+//show command message
+socket.on("tweet", function (tweet) {
+  console.log(tweet)
+  console.log('hij doet t')
+});
