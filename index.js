7d72ea21bf52564e620c60d179e8acd12d85edc2
Monika <monika_streljajeva@hotmail.com>
2020-05-02 20:21:12 +0200

    combined form data with api data & saved it on the server

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
-const express = require("express")
-const app = express()
-const http = require("http").Server(app)
-const io = require("socket.io")(http)
-const bodyParser = require("body-parser")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+require("dotenv").config();
 
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-app.use(express.static("public"))
-app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const KEY = process.env.KEY;
 
 app.get("/", function (req, res) {
-  res.render("pages/index", {})
-})
+  res.render("pages/index", {});
+});
 
-let history = []
-let rooms = []
-
+let history = [];
 
 io.sockets.on("connection", function (socket) {
+  console.log("someone connected");
 
-  socket.emit('test', 'Cow goes moo');
+  socket.emit("test", "joe joe");
+
+  // fetch("https://nookipedia.com/api/today/?api_key=" + KEY)
+  //   .then((res) => res.json())
+  //   .then((data) => socket.emit("test", data))
+  //   .catch((error) => console.error("Error:", error));
 
   socket.on("turnip message", function (data) {
-    io.emit("turnip message", data);
-    storeData(data)
+    fetch(
+      "https://nookipedia.com/api/villager/" +
+        data.villager +
+        "/?api_key=" +
+        KEY
+    )
+      .then((res) => res.json())
+      .then(function (data) {
+        let cleanedData = {
+          villagerImage: data.image,
+        };
+
+        let getApiData = combineData(cleanedData);
+      });
+
+    function combineData(getApiData) {
+      let formData = data;
+      let apiData = getApiData;
+      let combined = Object.assign(formData, apiData);
+      getFormData(combined);
+      io.emit("turnip message", combined);
+    }
   });
 
-  socket.on("today", function (data) {
-    io.emit("today", data);
-  });
-
-  socket.emit("turnip board", history)
+  socket.emit("turnip board", history);
 
   socket.on("disconnect", function () {
-    console.log("someone disconnected")
-  })
-})
+    console.log("someone disconnected");
+  });
+});
 
-function storeData(data) {
-  history.unshift(data)
-  console.log(history)
+function getFormData(combined) {
+  history.unshift(combined);
 }
 
 http.listen(process.env.PORT || 3000);
--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 const socket = io();
 const addBtn = document.querySelector("#add");
 const addIsland = document.querySelector("#addIsland");
-const feed = document.getElementById('feed')
-const dailyMessage = document.getElementById('dailyMessage')
+const feed = document.getElementById("feed");
+const dailyMessage = document.getElementById("dailyMessage");
 
 addBtn.addEventListener("click", function (e) {
-  console.log('click!')
-  document.querySelector(".form").classList.toggle("visible");;
+  console.log("click!");
+  document.querySelector(".form").classList.toggle("visible");
 });
 
 //add Island info
@@ -14,51 +14,44 @@ addIsland.addEventListener("submit", function (e) {
   e.preventDefault();
 
   let data = {
-    islandName: document.querySelector('#IslandName').value,
-    turnipPrice: document.querySelector('#TurnipPrice').value,
-    fruitType: document.querySelector('#fruit').value,
-    villager: document.querySelector('#villager').value,
-    dodoCode: document.querySelector('#dodoCode').value,
-    time: new Date().toLocaleString()
-  }
+    islandName: document.querySelector("#IslandName").value,
+    turnipPrice: document.querySelector("#TurnipPrice").value,
+    fruitType: document.querySelector("#fruit").value,
+    villager: document.querySelector("#villager").value,
+    dodoCode: document.querySelector("#dodoCode").value,
+    time: new Date().toLocaleString(),
+  };
 
-  document.querySelector(".form").classList.toggle("visible");;
+  document.querySelector(".form").classList.toggle("visible");
   socket.emit("turnip message", data);
 });
 
-socket.on('test', function (data) {
+socket.on("test", function (data) {
+  console.log(data);
   let html = `
                 <div class='daily'>
-                <li>${data}</li>
-                </div>`
-  dailyMessage.insertAdjacentHTML('afterbegin', html)
-})
+                <p>${data}</p>
+                <li>${data.events}</li>
+                <img src="${data.villager_images}" alt="Girl in a jacket">
+                </div>`;
+  dailyMessage.insertAdjacentHTML("afterbegin", html);
+});
 
 socket.on("turnip message", function (data) {
+  console.log(data);
   let html = `
                 <div class='card'>
                 <li>Island : ${data.islandName}</li>
                 <li>Date: ${data.time}</li>
                 <li>Turnip price: ${data.turnipPrice}</li>
-                </div>`
-  feed.insertAdjacentHTML('afterbegin', html)
-})
-
-socket.on("today", function (data) {
-  let html = `
-                <div class='card'>
-                <li>Island: ${data.islandName}</li>
-                <li>Date: ${data.time}</li>
-                <li>Turnip price: ${data.turnipPrice}</li>
-                </div>`
-  feed.insertAdjacentHTML('afterbegin', html)
-})
-
+                </div>`;
+  feed.insertAdjacentHTML("afterbegin", html);
+});
 
 socket.on("turnip board", function (data) {
-  console.log(data)
+  console.log(data);
 
-  feed.innerHTML = ''
+  feed.innerHTML = "";
 
   data.forEach(function (data) {
     let html = `
@@ -66,7 +59,7 @@ socket.on("turnip board", function (data) {
                 <li>Island: ${data.islandName}</li>
                 <li>Date: ${data.time}</li>
                 <li>Turnip price: ${data.turnipPrice}</li>
-                </div>`
-    feed.insertAdjacentHTML('beforeend', html)
-  })
+                </div>`;
+    feed.insertAdjacentHTML("beforeend", html);
+  });
 });
